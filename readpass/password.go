6854b4327dc398d2bf61f7c91b1bb264ea352a4d
Kyle <kyle@tyrfingr.is>
2014-05-19 00:13:30 -0600

    By default, print to stderr.

--- readpass/password.go
+++ readpass/password.go
@@ -6,13 +6,14 @@ package readpass
 // #include "noecho.h"
 import "C"
 import "fmt"
+import "os"
 import "unsafe"
 
 // ReadPass shows the prompt, and reads the password while blanking
 // the console. Most users will want to print a newline immediately
 // after reading the password.
 func ReadPass(prompt string) ([]byte, error) {
-	fmt.Printf("%s", prompt)
+	fmt.Fprintf(os.Stderr, "%s", prompt)
 	pw := C.readpass()
 	if pw == nil {
 		return nil, fmt.Errorf("readpass: failed to read password")

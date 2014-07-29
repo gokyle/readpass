de51ae9c52f5c08243ba7f42f1d9ef24d9582a6b
Kyle Isom <kyle@cloudflare.com>
2014-07-29 07:15:55 -0700

    No more cgo. Fixes #1.

--- password.go
+++ password.go
@@ -1,15 +1,28 @@
 package readpass
 
-// This file contains utillity functions for decrypting password protecting keys
+// This file contains utility functions for decrypting password protecting keys
 // and password protecting keys.
 
 import (
-	"bufio"
-	"bytes"
-	"fmt"
+	"log"
 	"os"
-	"strings"
+
+	"code.google.com/p/go.crypto/ssh/terminal"
 )
+
+func SSHPasswordPrompt(prompt string) (password string, err error) {
+	state, err := terminal.MakeRaw(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer terminal.Restore(0, state)
+	term := terminal.NewTerminal(os.Stdout, ">")
+	password, err = term.ReadPassword(prompt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
 
 // The PasswordPrompt function is the function that is called to prompt the user for
 // a password.
@@ -22,25 +35,25 @@ var PasswordPromptBytes func(prompt string) (password []byte, err error) = Defau
 // DefaultPasswordPrompt is a simple (but echoing) password entry function
 // that takes a prompt and reads the password.
 func DefaultPasswordPrompt(prompt string) (password string, err error) {
-	fmt.Printf(prompt)
-	rd := bufio.NewReader(os.Stdin)
-	line, err := rd.ReadString('\n')
+	state, err := terminal.MakeRaw(0)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
-	password = strings.TrimSpace(line)
+	defer terminal.Restore(0, state)
+	term := terminal.NewTerminal(os.Stdout, ">")
+	password, err = term.ReadPassword(prompt)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return
 }
 
 // DefaultPasswordPrompt is a simple (but echoing) password entry function
 // that takes a prompt and reads the password.
 func DefaultPasswordPromptBytes(prompt string) (password []byte, err error) {
-	fmt.Printf(prompt)
-	rd := bufio.NewReader(os.Stdin)
-	password, err = rd.ReadBytes('\n')
-	if err != nil {
-		return
+	passwordString, err := PasswordPrompt(prompt)
+	if err == nil {
+		password = []byte(passwordString)
 	}
-	password = bytes.TrimSpace(password)
 	return
 }
